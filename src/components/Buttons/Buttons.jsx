363d710d116338993cdd8a5c6f74c4d95420e98b
Matheus Morete Espinoso <128860497+MatheusMoorete@users.noreply.github.com>
2023-11-20 19:31:20 -0300

    Botão
    Coloquei um box shadow no botão da pagina Prevenção

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -17,6 +17,7 @@ const StyledButton = styled(motion.button)`
   cursor: pointer;
   font-size: 16px;
   border-radius: 5px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.184) 0px 18px 36px -18px inset;
 `;
 
 const Buttons = ({ children }) => {
